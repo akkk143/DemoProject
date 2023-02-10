2a879423c2b40b3ae7f6c7f1eae27e9e2dca2950
Ashok <ashokk@tekion.com>
2023-02-10 11:31:40 +0530

    added post api

--- app/app.go
+++ app/app.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
 	"log"
@@ -10,9 +11,17 @@ import (
 const serviceRoute = "demo/project"
 
 var srv *http.Server
+var messages string
+
+type message struct {
+	ServiceName     string `json:"service_name"`
+	Reason          string `json:"reason"`
+	TransactionType string `json:"transaction_type"`
+}
 
 // Start starts the http server
 func Start() {
+	messages = "Hello World!"
 	createServer()
 	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 		fmt.Println(err)
@@ -23,12 +32,25 @@ func Start() {
 func createServer() {
 	r := mux.NewRouter().StrictSlash(true)
 	r.HandleFunc(serviceRoute+"/printmsg", printMsg).Methods("GET")
+	r.HandleFunc(serviceRoute+"/alert", alert).Methods("POST")
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
 func printMsg(w http.ResponseWriter, r *http.Request) {
-	_, err := w.Write([]byte("Hello World"))
+	_, err := w.Write([]byte(messages))
 	if err != nil {
 		log.Printf("couldnt write response error [%s]\n", err)
+	}
+}
+func alert(w http.ResponseWriter, r *http.Request) {
+	msg := message{}
+	err := json.NewDecoder(r.Body).Decode(&msg)
+	str := fmt.Sprintf(msg.ServiceName, msg.Reason, msg.TransactionType)
+	messages = str
+	_, err = w.Write([]byte(str))
+	if err != nil {
+		log.Printf("couldnt write response error [%s]\n", err)
+	} else {
+		log.Println(str)
 	}
 }
