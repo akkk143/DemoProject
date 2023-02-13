0f1cae511c794a653319eb7917bcb5b952127d35
Ashok <ashokk@tekion.com>
2023-02-13 14:59:26 +0530

    added post api

--- app/app.go
+++ app/app.go
@@ -89,7 +89,7 @@ func customeQueryAlert(w http.ResponseWriter, r *http.Request) {
 	log.Println(msg)
 	//str := fmt.Sprintf("Alert : %s, RuleQuery : %s, RuleIndex : %s, ResultLink : %s, ResponseActions : %s", msg.Alerts, msg.RuleQuery, msg.RuleIndex,
 	//	msg.ResultLink, msg.ResponseActions)
-	str := fmt.Sprintf(msg.ResultLink + msg.msgs)
+	str := fmt.Sprintf(msg.ResultLink)
 	if len(messages) > 2 {
 		messages = make([]string, 0)
 	}
